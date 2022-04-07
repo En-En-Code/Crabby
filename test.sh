a153304adba788b93d4b0a3216375a50d5f3d141
En-En <39373446+En-En-Code@users.noreply.github.com>
2022-04-07 12:52:39 -0400

    Generalized test.sh to run w/o modding

--- test.sh
+++ test.sh
@@ -1,12 +1,23 @@
+#!/bin/sh
+
 mem=$(free|awk '/^Mem:/{print $4}')
 nthreads=$(nproc)
 memper=$((mem*4/5/nthreads/1024/2))
-echo $memper
-output=/home/alex/Dropbox/workspace/rust/chess/results/games.pgn
-book=/home/alex/Dropbox/workspace/rust/chess/info/ecoe.pgn
 
-cutechess-cli -fcp cmd=$1 proto=uci initstr="setoption name hash value $memper" \
--scp cmd=$2 proto=uci initstr="setoption name hash value $memper" \
--both tc=40/60 timemargin=10000000 \
--games 2000 -repeat -recover \
--concurrency $nthreads -pgnin $book -pgnout $output
+echo $memper
+if [ ! -e testing/results ];
+then mkdir ./testing/results
+fi
+output=./testing/results/games.pgn
+book=./testing/book.pgn
+
+engine1=${3:-'./target/release/crabby'}
+engine2=${2:-'./target/release/crabby'}
+numgames=${1:-'2000'}
+
+cutechess-cli \
+-engine cmd=$engine1 proto=uci initstr="setoption name hash value $memper" \
+-engine cmd=$engine2 proto=uci initstr="setoption name hash value $memper" \
+-each tc=40/60 timemargin=0 \
+-games $numgames -repeat -recover \
+-concurrency $nthreads -openings file=$book -pgnout $output
